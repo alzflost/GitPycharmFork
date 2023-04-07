c83a7d7e01b702655a019b79f2bcfc9c91e1b7ad
Hyunseo Kim <alzflost@naver.com>
2023-04-07 13:30:16 +0900

    crawl code

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-#
-#
-#
-print("Hello World 1 in team leader server")
+import requests
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+import urllib3
+from urllib3.exceptions import InsecureRequestWarning
+urllib3.disable_warnings(InsecureRequestWarning)
 
-print("Update in leader")
+context = ssl._create_unverified_context()
 
-print("Update in local C")
+url = 'https://www.sogang.ac.kr/front/boardlist.do?bbsConfigFK=2'
 
-print(“ Update in local B”)
+response = requests.get(url, verify=False)
 
-print("Update in local A")
+if response.status_code == 200:
+    html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
+    #print(soup)
+    title = soup.select('a')
+    #title = soup.select_one('body > div > div > div:nth-child(2) > div > table > tbody > tr:nth-child(1) > td:nth-child(2) > div > a')
+    for i in title:
+        print("\n\n공지\n")
+        print(i)
+else:
+    print(response.status_code)
